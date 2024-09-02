f89837a2729b48f4c2d44bd50a082c01a76a39ab
KowshikaK10 <kowshi010101@gmail.com>
2024-09-02 11:40:44 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,42 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+import menuItems from '../Data/Data'
+const App = () => {
+    const [items,setItems]=useState(menuItems);
+    function handleFilter(filterType){
+        if(filterType==='all'){
+            setItems(menuItems)
+        }else{
+            const newItems=menuItems.filter(item=> item.category===filterType);
+            setItems(newItems)
+        }
+        
+    }
+  return (
+    <div>
+      <h1> Our Menu</h1>
+      <div className="btn_container">
+        <button onClick={()=>handleFilter('all')}>ALL</button>
+        <button onClick={()=>handleFilter('breakfast')}>Breakfast</button>
+        <button onClick={()=>handleFilter('lunch')}>Lunch</button>
+        <button onClick={()=>handleFilter('shakes')}>Shakes</button>
+      </div>
+       <div className="menu">
+        {items.map((menuItem)=>{
+             const { id, title, img, desc, price, category } = menuItem;
+            return(
+                <div id={id}>
+                    <img src={img} alt={title}/>
+                    <header>
+                    <h4>{title}</h4>
+                    <h4 className="price">${price}</h4>
+                    </header>
+                    <p className="item-text">{desc}</p>
+                </div>
+             )
+            })}
+       </div>
+    </div>
+  )
+}
 
+export default App
